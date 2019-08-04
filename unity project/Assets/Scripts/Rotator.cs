eba7535e644aa7c045d4d7e025d21a6d84a25c1d
keel-210 <crafters_touhou@yahoo.co.jp>
2019-08-04 21:16:00 +0900

    Stage_Base

---
+++ unity project/Assets/Scripts/Rotator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField] Vector3 Rot;
+    [SerializeField] AnimationCurve RotRatio;
+    [SerializeField] float RotLength;
+    void Update()
+    {
+        transform.rotation *= Quaternion.Euler(Rot * RotRatio.Evaluate(Mathf.Repeat(Time.time / RotLength, 1)));;
+    }
+}
